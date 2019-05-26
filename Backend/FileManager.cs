3ecc2e76cb7a08dc24d0440149db231af692342c
Andrew Sabato <andrewsabato8@gmail.com>
2019-05-26 16:43:51 +1000

    Nothing

--- Backend/FileManager.cs
+++ Backend/FileManager.cs
@@ -15,6 +15,7 @@ namespace Backend
 
 		public const string ObjdumpPath = "objdump";
 		public const string PythonPath = "/usr/bin/python3";
+		// Andrew's Python
 		// public const string PythonPath = "/Users/AndrewSabato/anaconda3/bin/python";
 
 		public const string CreateCSVPath = "create_csv.py";
